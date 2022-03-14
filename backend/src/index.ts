a6677b4cbffd4d4b4dd8ca2529f7f7dd6b3587b3
ih par <ismail@cs.deu.edu.tr>
2022-03-14 20:52:47 +0300

    basic server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hi");
+import express from "express";
+import bodyParser from "body-parser";
+
+import userRoutes from "./routes/user-routes";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(userRoutes);
+
+app.listen(8000);
