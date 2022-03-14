a2feb55644afac7e9b52f6e4a5c5bf49961b27d3
ih par <ismail@cs.deu.edu.tr>
2022-03-14 10:22:36 +0300

    arrow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import classes from "./App.module.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <p className={classes.p}>Hi</p>
     </div>
   );
-}
+};
 
 export default App;
