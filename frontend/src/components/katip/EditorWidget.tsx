cedc87981065f78614a1615c2b0fcb266a196451
ih par <ismail@cs.deu.edu.tr>
2022-03-18 16:45:24 +0300

    adding fonts

--- frontend/src/components/katip/EditorWidget.tsx
+++ frontend/src/components/katip/EditorWidget.tsx
@@ -1,10 +1,21 @@
-import classes from "./EditorWidget.module.scss";
+import "./EditorWidget.scss";
 
 const EditorWidget: React.FC = (props) => {
   return (
-    <div className={classes["accordion-wrapper"]}>
-      <p>Hi</p>
-      {props.children}
+    <div className="accordion-wrapper">
+      <button className="btn-accordion accordion-active">
+        <span className="i-sharp caret">arrow_right</span>
+        Makam
+      </button>
+      <div className="accordion-panel">
+        <div className="accordion-contents">
+          <div className="content-scroller makam">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, illo accusantium ut minima veritatis
+            maiores libero cum. Aperiam voluptas, harum sunt vel sint quibusdam numquam rerum architecto accusantium,
+            illo a.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
