f6713b59b18af4a536a6243acd126da103add2b3
ih par <ismail@cs.deu.edu.tr>
2022-03-18 12:20:56 +0300

    added routes logic to backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const env = require("dotenv");
 
+const usersRoutes = require("./routes/users-routes");
+
 app = express();
+
+app.use(bodyParser.json());
+
+app.use(usersRoutes);
+
 env.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT);
