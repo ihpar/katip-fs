61f23e3ac97a967445653d0efa7d7ece7fe3cec1
ih par <ismail@cs.deu.edu.tr>
2022-03-22 12:00:24 +0300

    added sheet music

--- frontend/src/components/katip/NoteSheet.tsx
+++ frontend/src/components/katip/NoteSheet.tsx
@@ -1,7 +1,93 @@
+import "./NoteSheet.scss";
+
 const NoteSheet = () => {
   return (
     <div className="A4">
-      <div className="paper-contents"></div>
+      <div className="paper-contents">
+        <h1 className="song-title">Eserin Başlığı</h1>
+        <h2 className="composer">Besteci Ad Soyad</h2>
+        <h3 className="bpm-section">
+          <span className="duration dur-8"></span>
+          <span className="normal-text">=</span>
+          <span>150</span>
+        </h3>
+        <div>
+          <div className="music-line incomplete">
+            <div className="measure">
+              <div className="staff-lines">
+                <div className="staff-space"></div>
+                <div className="staff-top"></div>
+                <div className="staff-space"></div>
+                <div className="staff-top"></div>
+                <div className="staff-space"></div>
+                <div className="staff-top"></div>
+                <div className="staff-space"></div>
+                <div className="staff-top"></div>
+                <div className="staff-space"></div>
+                <div className="staff-top"></div>
+                <div className="staff-space"></div>
+                <div className="staff-middle"></div>
+                <div className="staff-space"></div>
+                <div className="staff-middle"></div>
+                <div className="staff-space"></div>
+                <div className="staff-middle"></div>
+                <div className="staff-space"></div>
+                <div className="staff-middle"></div>
+                <div className="staff-space"></div>
+                <div className="staff-middle"></div>
+                <div className="staff-space"></div>
+                <div className="staff-bottom"></div>
+                <div className="staff-space"></div>
+                <div className="staff-bottom"></div>
+                <div className="staff-space"></div>
+                <div className="staff-bottom"></div>
+                <div className="staff-space"></div>
+                <div className="staff-bottom"></div>
+                <div className="staff-space"></div>
+                <div className="staff-bottom"></div>
+                <div className="staff-space"></div>
+              </div>
+              <div className="music-symbols">
+                <span className="clef">&#xE050;</span>
+                <div className="key-signature">
+                  <span className="g4">&#xE440;</span>
+                  <span className="c5">&#xE284;</span>
+                  <span className="f5">&#xE447;</span>
+                </div>
+                <div className="time-signature">
+                  <div className="numerator">
+                    <span className="num-9"></span>
+                  </div>
+                  <div className="denominator">
+                    <span className="num-8"></span>
+                  </div>
+                </div>
+                <div className="notes-wrapper">
+                  <svg className="measure-svg">
+                    <text fontSize="46" fontFamily="BravuraText" fill="#222222">
+                      <tspan x="0" y="83">
+                        &#xE1D6;
+                      </tspan>
+                      <tspan x="25" y="93">
+                        &#xE1D6;
+                      </tspan>
+                      <tspan x="50" y="88">
+                        &#xE1D6;
+                      </tspan>
+                      <tspan x="75" y="98">
+                        &#xE1DA;
+                      </tspan>
+                      <tspan x="100" y="103">
+                        &#xE1DB;
+                      </tspan>
+                    </text>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
